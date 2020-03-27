65ee2eae6f16bcb98362571b3efc9c52dae963d9
Nathan Altice <naltice@ucsc.edu>
2020-03-27 16:01:59 -0700

    Paddle physics implemented

--- src/main.js
+++ src/main.js
@@ -7,11 +7,21 @@
 
 // define and configure main Phaser game object
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.AUTO,
     height: 640,
     width: 960,
     scale: {
         autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
     },
     scene: [ Load, Title, Play, GameOver ]
 }
@@ -26,10 +36,6 @@ let paddle = null;
 let paddleVelocity = 150;
 const colors = [0x1BE7ff, 0x6EEB83, 0xE4FF1A, 0xE8AA14]; // colors array (rainbows!)
 let colorIndex = 0;
-let extremeMODE;
-let shadowLock = false;
-let barrierSpeed;
-let level;
 let highScore;
 let newHighScore;
 let cursors;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -3,18 +3,93 @@ class Play extends Phaser.Scene {
         super('playScene');
     }
 
-    preload() {
-
-    }
-
     create() {
+        // reset parameters
+        this.barrierSpeed = -450;
+        this.level = 0;
+        this.extremeMODE = false;
+        this.shadowLock = false;
+
+        // set up audio, play bgm
+
         // set up cursor keys
         cursors = this.input.keyboard.createCursorKeys();
+
+        // set up paddle (physics sprite)
+        paddle = this.physics.add.sprite(32, centerY, 'paddle').setOrigin(0.5);
+        paddle.setCollideWorldBounds(true);
+        paddle.setBounce(0.5);
+        paddle.setImmovable();
+        paddle.setMaxVelocity(0, 600);
+        paddle.setDragY(200);
+        paddle.setDepth(1);         // ensures that paddle z-depth remains above shadow paddles
+        paddle.destroyed = false;   // custom property to track paddle life
+
+        // set up barrier group
+
+
+        // set up difficulty timer
+        this.difficultyTimer = this.time.addEvent({
+            delay: 1000,
+            callback: this.speedBump,
+            callbackScope: this,
+            loop: true
+        });
     }
 
     update() {
-        if (Phaser.Input.Keyboard.JustDown(cursors.up)) {
-            console.log('key up');
+        // check for player input
+        if(cursors.up.isDown) {
+            paddle.body.velocity.y -= paddleVelocity;
+        } else if(cursors.down.isDown) {
+            paddle.body.velocity.y += paddleVelocity;
         }
+
+        // check for collision
+        if(!paddle.destroyed) {
+
+        }
+
+        // spawn rainbow trail if in EXTREME mode
+        if(this.extremeMODE && !this.shadowLock && !paddle.destroyed) {
+            this.spawnShadowPaddles();
+            this.shadowLock = true;
+            // lock shadow paddle spawning to a given time interval
+            this.time.delayedCall(15, () => { this.shadowLock = false; })
+        }
+    }
+
+    speedBump() {
+        // raise barrier speed, increment level
+        this.barrierSpeed -= 10;
+        this.level++; console.log(`level: ${ this.level }`);
+
+        // set HARD mode
+        if(this.level == 30) {
+            paddle.scaleY = 0.75;
+        }
+        // set EXTREME mode
+        if(this.level == 60) {
+            paddle.scaleY = 0.5;
+            this.extremeMODE = true;
+        }
+    }
+
+    spawnShadowPaddles() {
+        // add a "shadow paddle" at main paddle position
+        let shadowPaddle = this.add.image(paddle.x, paddle.y, 'paddle').setOrigin(0.5);
+        shadowPaddle.scaleY = paddle.scaleY;            // scale to parent paddle
+        shadowPaddle.tint = Math.random() * 0xFFFFFF;   // tint w/ rainbow colors
+        shadowPaddle.alpha = 0.5;                       // make semi-transparent
+        // tween alpha to 0
+        this.tweens.add({ 
+            targets: shadowPaddle, 
+            alpha: { from: 0.5, to: 0 }, 
+            duration: 750,
+            ease: 'Linear',
+            repeat: 0 
+        });
+        // set a kill timer for trail effect
+        this.time.delayedCall(750, () => { shadowPaddle.destroy(); } );
     }
 }
